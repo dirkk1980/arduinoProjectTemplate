e23f64b900b9b7d6e215676fdace490dfc6af23e
quallenbezwinger <dirkkoehler1980@googlemail.com>
2014-06-25 20:30:47 +0200

    new formatter and cleanup

--- main.cpp
+++ main.cpp
@@ -6,31 +6,20 @@
  */
 #include <Arduino.h>
 
-int led = 12;
-
-// The setup() method runs once, when the sketch starts
-void setup() {
-	// initialize the digital pin as an output:
-	pinMode(led, OUTPUT);
+void setup()
+{
 }
 
-// the loop() method runs over and over again,
-// as long as the Arduino has power
-
-void loop() {
-	digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);          // wait for a second               // wait for a second
-	digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);          // wait for a second               // wait for a second
+void loop()
+{
 }
 
-int main(void) {
-
-	/* Must call init for arduino to work properly */
+int main(void)
+{
 	init();
 	setup();
-
-	for (;;) {
+	for (;;)
+	{
 		loop();
-	} // end for
-} // end main
+	}
+}
